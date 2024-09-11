1b3ba86fd0ca6e3d2084b1c49f26e04ddb4c1b5c
Harsh <harsh09raj@gmail.com>
2024-09-11 10:47:25 +0530

    1st commit

--- TRUTHLIE.cpp
+++ TRUTHLIE.cpp
@@ -1,19 +1,68 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-#define int long long
 
-int32_t main() {
-	int T;
-	cin>>T;
-	while(T--)
+int main() {
+	
+	int t ;
+	cin >> t ;
+	
+	while(t--)
 	{
-	  int n,m;
-	  cin>>n>>m;
-	  if(n>m)
-	  cout<<2*m+1<<endl;
-	  else 
-	  cout<<-1<<endl;
-	  
+	    int n ;
+	    cin >> n ;
+	    
+	    int a[n], ev[n], od[n], e = 0, o = 0 ;
+	    
+	    bool fl = false ;
+	    for(int i = 0 ; i < n ; i++)
+	    {
+	        cin >> a[i] ;
+	        
+	        if(a[i]%2 == 0)
+	        {   e++ ;   ev[e-1] = i+1 ; }
+	        
+	        else if(a[i] != 1)
+	        {
+	            o++ ;
+	            
+	            od[o-1] = i+1 ;
+	            
+	        }
+	        
+	        else if(fl == false)
+	        {   o++ ; od[o-1] = i+1 ; fl = true ; }
+	        
+	    }
+	    
+	    if(e >= 2)
+	    cout << ev[0] << " " << ev[1] << endl ;
+	    
+	    else if(o >= 2)
+	    cout << od[0] << " " << od[1] << endl ;
+	    
+	    else
+	    {
+	        int tmp = a[0] + a[1] ;
+	        
+	        fl = false ;
+	        
+	        for (int i = 2; i <= std::sqrt(tmp); ++i) 
+	        {
+                if (tmp % i == 0) {
+                    fl = true;
+                    break;
+                }
+            }
+            
+            if(fl)
+            cout << 1 << " " << 2 << endl ;
+            
+            else
+            cout << -1 << endl ;
+	        
+	    }
+	    
+	    
 	}
-	return 0;
+
 }
